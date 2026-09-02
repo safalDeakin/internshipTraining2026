2f3b53d7a0f2fcd16f3e9f09324ad7f90b5435d1
Shrajja Ghimire <shrajjaghimire@gmail.com>
2026-09-02 10:47:44 +0545

    Add useOffer hook

--- src/hooks/useOffer.ts
+++ src/hooks/useOffer.ts
@@ -3,15 +3,14 @@ import { offerState } from "../store/OfferState";
 import { offerActions } from "../store/OfferActions";
 
 export const useOffer = () => {
-    const state = useSyncExternalStore(
-        (listener) => offerState.subscribe(listener),
-        () => offerState.getSnapshot()
-    );
+  const state = useSyncExternalStore(
+    (listener) => offerState.subscribe(listener),
+    () => offerState.getSnapshot(),
+  );
 
-    return {
-        filteredOffers: state.filteredOffers,
-
-        setSearch: offerActions.setSearch.bind(offerActions),
-        addOffer: offerActions.addOffer.bind(offerActions),
-    };
+  return {
+    filteredOffers: state.filteredOffers,
+    setSearch: offerActions.setSearch.bind(offerActions),
+    addOffer: offerActions.addOffer.bind(offerActions),
+  };
 };
